b88c4821181451b30d9dbbd4c67e757e951e969c
thanhn <510thanh.ngo@gmail.com>
2020-05-12 20:49:23 -0700

    burgers controller

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -23,19 +23,9 @@ router.post("/burgers/create", function(req, res) {
 });
 
 router.put("/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  burger.update(req.params.id, function(result) {
+    console.log(result);
+    res.sendStatus(200);
   });
 });
 
