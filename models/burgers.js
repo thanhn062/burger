42243a8abd1001d26fbee9ebb600c4769e6eacec
thanhn <510thanh.ngo@gmail.com>
2020-05-12 20:47:13 -0700

    new files

---
+++ models/burgers.js
@@ -0,0 +1,27 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+  create: function(name, cb) {
+    orm.create("burgers",
+              ["burger_name","devoured"],
+              [name, false],
+              cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers",
+              {devoured: true},
+              condition,
+              cb);
+  },
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
